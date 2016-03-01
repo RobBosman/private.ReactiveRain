6faeff4beb0c6f208ea47f88e46e6d0ec4d16af8
Rob Bosman <rob.bosman.nl+github@gmail.com>
2016-03-01 01:00:58 +0100

    RxObservable??

--- src/main/java/nl/bransom/vertex/Main.java
+++ src/main/java/nl/bransom/vertex/Main.java
@@ -3,11 +3,10 @@ package nl.bransom.vertex;
 import io.vertx.core.Vertx;
 import io.vertx.core.VertxOptions;
 import io.vertx.core.eventbus.EventBus;
-import io.vertx.core.shareddata.AsyncMap;
-import io.vertx.core.shareddata.LocalMap;
-import io.vertx.core.shareddata.SharedData;
+import io.vertx.rx.java.RxHelper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import rx.Observable;
 
 public class Main {
 
@@ -22,12 +21,7 @@ public class Main {
       final VertxOptions options = new VertxOptions();
       Vertx.clusteredVertx(options, res -> {
         if (res.succeeded()) {
-          final Vertx vertx = res.result();
-
-          doVertex(vertx);
-
-          putCvJsonOnClusterWideMap(vertx, "JSON DATA CLUSTERED");
-          vertx.setTimer(100, timerId -> getCvJsonFromClusterWideMap(vertx));
+          doVertex(res.result());
         } else {
           LOG.error("Error getting clustered vertx.", res.cause());
         }
@@ -55,9 +49,26 @@ public class Main {
       });
     });
 
+    final Observable<String> observableDeploymentId = RxHelper.observableHandler();
+    observableDeploymentId.subscribe(value -> {
+      final String deploymentId = value;
+      LOG.debug("Deployment id from result is: " + deploymentId);
+      vertx.setTimer(6000, timerId ->
+          vertx.undeploy(deploymentId, res2 -> {
+            if (res2.succeeded()) {
+              LOG.info("shut down " + deploymentId + " OK");
+            } else {
+              LOG.error("shut down " + deploymentId + " failed", res2.cause());
+            }
+          })
+      );
+    });
+
     vertx.deployVerticle(MyVerticle.class.getName(), res -> {
       if (res.succeeded()) {
         final String deploymentId = res.result();
+//        observableDeploymentId.; // TODO - ?
+        observableDeploymentId.startWith(deploymentId);
         vertx.eventBus().publish("deployed verticle", deploymentId);
         LOG.info("Deployment id is: " + deploymentId);
       } else {
@@ -78,65 +89,6 @@ public class Main {
           })
       );
     });
-
-
-    putCvJsonOnLocalSharedMap(vertx, "JSON DATA LOCAL");
-    String jsonData = getCvJsonFromLocalSharedMap(vertx);
-    dealWithCvJson(jsonData);
-  }
-
-  private static void putCvJsonOnLocalSharedMap(final Vertx vertx, final String jsonData) {
-    final SharedData sd = vertx.sharedData();
-    final LocalMap<String, String> map = sd.<String, String>getLocalMap("cvtool");
-    map.put("cv", jsonData);
-  }
-
-  private static String getCvJsonFromLocalSharedMap(final Vertx vertx) {
-    final SharedData sd = vertx.sharedData();
-    final LocalMap<String, String> map = sd.<String, String>getLocalMap("cvtool");
-    return map.get("cv");
-  }
-
-
-  private static void putCvJsonOnClusterWideMap(final Vertx vertx, final String jsonData) {
-    vertx.sharedData().<String, String>getClusterWideMap("cvtool", res -> {
-      if (res.succeeded()) {
-        final AsyncMap<String, String> map = res.result();
-        map.put("cv", jsonData, h -> {
-          if (h.succeeded()) {
-            LOG.info("Data was put on cluster wide map");
-          } else {
-            LOG.error("Putting data on cluster wide map failed!", res.cause());
-          }
-        });
-      } else {
-        LOG.error("Getting cluster wide map failed!", res.cause());
-      }
-    });
-  }
-
-  private static void getCvJsonFromClusterWideMap(final Vertx vertx) {
-    vertx.sharedData().<String, String>getClusterWideMap("cvtool", res -> {
-      if (res.succeeded()) {
-        getCvJsonFromMap(res.result());
-      } else {
-        LOG.error("Getting clustered shared data failed!", res.cause());
-      }
-    });
-  }
-
-  private static void getCvJsonFromMap(final AsyncMap<String, String> map) {
-    map.get("cv", res -> {
-      if (res.succeeded()) {
-        dealWithCvJson(res.result());
-      } else {
-        LOG.error("Getting data from clustered shared data failed!", res.cause());
-      }
-    });
-  }
-
-  private static void dealWithCvJson(final String cvJson) {
-    LOG.info("Got it: " + cvJson);
   }
 
   public static <T> void handleThat(final T t, final Handler<T> handler) {
--- src/main/java/nl/bransom/vertex/MyVerticle.java
+++ src/main/java/nl/bransom/vertex/MyVerticle.java
@@ -1,6 +1,10 @@
 package nl.bransom.vertex;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Vertx;
+import io.vertx.core.shareddata.AsyncMap;
+import io.vertx.core.shareddata.LocalMap;
+import io.vertx.core.shareddata.SharedData;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,21 +16,26 @@ public class MyVerticle extends AbstractVerticle {
   public void start() {
     LOG.info(getClass().getName() + " - deployed");
 
-    vertx.eventBus().consumer("news.uk.sport")
-        .handler(message -> {
+    if (vertx.isClustered()) {
+      putCvJsonOnClusterWideMap(vertx, "JSON DATA CLUSTERED");
+      vertx.setTimer(100, timerId -> getCvJsonFromClusterWideMap(vertx));
+    }
+
+    putCvJsonOnLocalSharedMap(vertx, "JSON DATA LOCAL");
+    String jsonData = getCvJsonFromLocalSharedMap(vertx);
+    dealWithCvJson(jsonData);
+
+    vertx.eventBus()
+        .consumer("news.uk.sport", message -> {
           LOG.info("received message: " + message.body());
           message.reply("how interesting!");
         });
 
     vertx.createHttpServer()
-        .requestHandler(request -> {
-          request.handler(buffer -> LOG.debug("received some bytes: " + buffer.toString()));
-
-          request.bodyHandler(totalBuffer -> LOG.debug("Full body received, path = " + request.path() + ", length = " + totalBuffer.length()));
-
-          request.response().end("Hello world");
-
-        })
+        .requestHandler(request -> request
+              .handler(buffer -> LOG.debug("received some bytes: " + buffer.toString()))
+              .bodyHandler(totalBuffer -> LOG.debug("Full body received, path = " + request.path() + ", length = " + totalBuffer.length()))
+              .response().end("Hello world"))
         .listen(8080, "localhost", res -> {
           if (res.succeeded()) {
             LOG.info("Server is now listening on http://localhost:8080/");
@@ -39,5 +48,57 @@ public class MyVerticle extends AbstractVerticle {
   @Override
   public void stop() {
     LOG.info(getClass().getName() + " - undeployed");
+  }
+
+  private static void putCvJsonOnLocalSharedMap(final Vertx vertx, final String jsonData) {
+    final SharedData sd = vertx.sharedData();
+    final LocalMap<String, String> map = sd.<String, String>getLocalMap("cvtool");
+    map.put("cv", jsonData);
+  }
+
+  private static String getCvJsonFromLocalSharedMap(final Vertx vertx) {
+    final SharedData sd = vertx.sharedData();
+    final LocalMap<String, String> map = sd.<String, String>getLocalMap("cvtool");
+    return map.get("cv");
+  }
+  private static void putCvJsonOnClusterWideMap(final Vertx vertx, final String jsonData) {
+    vertx.sharedData().<String, String>getClusterWideMap("cvtool", res -> {
+      if (res.succeeded()) {
+        final AsyncMap<String, String> map = res.result();
+        map.put("cv", jsonData, h -> {
+          if (h.succeeded()) {
+            LOG.info("Data was put on cluster wide map");
+          } else {
+            LOG.error("Putting data on cluster wide map failed!", res.cause());
+          }
+        });
+      } else {
+        LOG.error("Getting cluster wide map failed!", res.cause());
+      }
+    });
+  }
+
+  private static void getCvJsonFromClusterWideMap(final Vertx vertx) {
+    vertx.sharedData().<String, String>getClusterWideMap("cvtool", res -> {
+      if (res.succeeded()) {
+        getCvJsonFromMap(res.result());
+      } else {
+        LOG.error("Getting clustered shared data failed!", res.cause());
+      }
+    });
+  }
+
+  private static void getCvJsonFromMap(final AsyncMap<String, String> map) {
+    map.get("cv", res -> {
+      if (res.succeeded()) {
+        dealWithCvJson(res.result());
+      } else {
+        LOG.error("Getting data from clustered shared data failed!", res.cause());
+      }
+    });
+  }
+
+  private static void dealWithCvJson(final String cvJson) {
+    LOG.info("Got it: " + cvJson);
   }
 }
