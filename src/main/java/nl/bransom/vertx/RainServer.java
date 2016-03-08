470b64dbe36288a2812774a44082f90bcffaf3d0
5 <5>
2016-03-08 16:36:38 +0100

    added Router

--- src/main/java/nl/bransom/vertx/RainServer.java
+++ src/main/java/nl/bransom/vertx/RainServer.java
@@ -2,6 +2,7 @@ package nl.bransom.vertx;
 
 import io.vertx.core.Future;
 import io.vertx.rxjava.core.AbstractVerticle;
+import io.vertx.rxjava.ext.web.Router;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,11 +12,24 @@ public class RainServer extends AbstractVerticle {
 
   @Override
   public void start(final Future<Void> startFuture) {
+
+    final Router router = Router.router(vertx);
+
+    router.route("/*")
+        .handler(routingContext -> {
+          routingContext.response()
+              // enable chunked responses because we will be adding data as
+              // we execute over other handlers. This is only required once and
+              // only if several handlers do output.
+              .setChunked(true)
+              .putHeader("content-type", "text/plain")
+              .write("Hello World via route1\n")
+              .end();
+          routingContext.next();
+        });
+
     vertx.createHttpServer()
-        .requestHandler(request -> request
-            .handler(buffer -> LOG.debug("received some bytes: " + buffer.toString()))
-            .bodyHandler(totalBuffer -> LOG.debug("Full body received, path = " + request.path() + ", length = " + totalBuffer.length()))
-            .response().end("Hello world"))
+        .requestHandler(router::accept)
         .listen(8080, res -> {
           if (res.succeeded()) {
             LOG.info("Server is now listening on http://localhost:8080/");
