788066b37fb3ec492e4ecbe856096806ad0769c6
5 <5>
2016-03-09 18:17:04 +0100

    improved RainMaker

--- src/main/java/nl/bransom/vertx/RainMaker.java
+++ src/main/java/nl/bransom/vertx/RainMaker.java
@@ -17,48 +17,60 @@ import java.util.Random;
 public class RainMaker extends AbstractVerticle {
 
   private static final Logger LOG = LoggerFactory.getLogger(RainMaker.class);
+  private static final String INTENSITY_TAG = "RainMaker.intensity";
+  private static final long MAX_INTERVAL_MILLIS = 3000;
 
   public static void startRaining(final Vertx vertx, final Future<Void> result) {
-    vertx.eventBus().send("RainMaker.intervalMillis", RainMaker.START, Main.handleFuture(result));
+    vertx.eventBus().send(INTENSITY_TAG, 1.0, Main.handleFuture(result));
   }
 
   public static void stopRaining(final Vertx vertx) {
-    vertx.eventBus().publish("RainMaker.intervalMillis", RainMaker.STOP);
+    vertx.eventBus().publish(INTENSITY_TAG, 0.0);
   }
 
-  public static final int START = 1;
-  public static final int STOP = 0;
+  private final Random random;
+  private Long timerID;
+
+  public RainMaker() {
+    random = new Random();
+  }
 
   @Override
   public void start() {
-    final Random random = new Random();
     final List<RainDrop> rainDrops = new ArrayList<>();
 
     final Subscriber<RainDrop> rds = Subscribers.create(rainDrop -> LOG.info("\t" + rainDrop));
 
     vertx.eventBus()
-        .<Integer>consumer("RainMaker.intervalMillis")
+        .<Double>consumer(INTENSITY_TAG)
         .toObservable()
         .doOnNext(message -> message.reply("OK"))
         .map(Message::body)
-        .subscribe(intervalMillis -> {
-          LOG.debug("RainMaker.intervalMillis = " + intervalMillis);
-          if (intervalMillis > 0) {
-            Observable
-                .<RainDrop>create(subscriber ->
-                    vertx.setPeriodic(intervalMillis, timerId -> {
-                      if (subscriber.isUnsubscribed()) {
-                        vertx.cancelTimer(timerId);
-                      } else {
-                        subscriber.onNext(new RainDrop(random));
-                      }
-                    }))
-                .doOnNext(rainDrops::add)
-                .subscribe(rds);
+        .map(intensity -> (long) ((1.0 - intensity) * MAX_INTERVAL_MILLIS))
+        .flatMap(this::createRainDropObservable)
+        .doOnNext(rainDrops::add)
+        .subscribe(rds);
+  }
+
+  private Observable<RainDrop> createRainDropObservable(final long intervalMillis) {
+    LOG.info("intervalMillis: " + intervalMillis);
+    if (intervalMillis < 0 || intervalMillis >= MAX_INTERVAL_MILLIS) {
+      if (timerID != null) {
+        vertx.cancelTimer(timerID);
+        timerID = null;
+      }
+      return Observable.empty();
+    } else {
+      return Observable.<RainDrop>create(subscriber -> {
+        final long delay = (intervalMillis > 0 ? intervalMillis : 1L) * 250;
+        timerID = vertx.setPeriodic(delay, timerId -> {
+          if (subscriber.isUnsubscribed()) {
+            vertx.cancelTimer(timerId);
           } else {
-            rds.unsubscribe();
-            LOG.info("Total RainDrops: " + rainDrops.size());
+            subscriber.onNext(new RainDrop(random));
           }
         });
+      });
+    }
   }
 }
