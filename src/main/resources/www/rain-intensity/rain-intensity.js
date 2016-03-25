c8b1cdb29239ab3164a435c743601f8f10b42d84
Rob Bosman <rob.bosman.nl+github@gmail.com>
2016-03-26 00:50:02 +0100

    cosmetic

--- src/main/resources/www/rain-intensity/rain-intensity.js
+++ src/main/resources/www/rain-intensity/rain-intensity.js
@@ -9,28 +9,19 @@ function RainIntensity() {
   this.initializeSlider = function() {
     rainIntensitySlider = document.getElementById('rain-intensity');
     noUiSlider.create(rainIntensitySlider, {
-      start: [0],
-      orientation: 'vertical',
-      direction: 'rtl',
-      range: {
-        'min': 0,
-        'max': 100
-      },
-      pips: {
-        mode: 'positions',
-        values: [0, 25, 50, 75, 100],
-        density: 4
+        start: 0,
+        orientation: 'vertical',
+        direction: 'rtl',
+        range: { 'min': 0, 'max': 100 },
+        pips: { mode: 'positions', values: [0, 25, 50, 75, 100], density: 4
       }
     });
 
     rainIntensitySlider.noUiSlider.on('update', function(values, handle) {
       if (!isSuppressingUpdates) {
-        eventBus.publish('rain.intensity.set', {
-            'value': values[handle] / 100.0
-          },
-          {
-            'id': ID
-          });
+        eventBus.publish('rain.intensity.set',
+          { 'value': values[handle] / 100.0 },
+          { 'id': ID });
       }
     });
 
