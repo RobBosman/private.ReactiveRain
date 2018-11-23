f85af0d39f653c75e3518fa712a4cacaa452b796
RobBosman <rob.bosman.nl+github@gmail.com>
2018-11-23 17:52:50 +0100

    polished code a bit more

--- src/test/java/nl/bransom/reactive/rain/RainServerTest.java
+++ src/test/java/nl/bransom/reactive/rain/RainServerTest.java
@@ -1,7 +1,6 @@
 package nl.bransom.reactive.rain;
 
 import io.vertx.core.Vertx;
-import io.vertx.ext.unit.Async;
 import io.vertx.ext.unit.TestSuite;
 import org.junit.jupiter.api.Test;
 
@@ -15,22 +14,19 @@ class RainServerTest {
 
     TestSuite
         .create("testSuite")
-        .before(testContext -> {
-          final Async async = testContext.async();
-          vertx
-              .exceptionHandler(testContext.exceptionHandler())
-              .deployVerticle(RainServer.class.getName(), testContext.asyncAssertSuccess(x ->
-                  async.complete()));
-        })
+        .before(testContext ->
+            vertx
+                .exceptionHandler(testContext.exceptionHandler())
+                .deployVerticle(RainServer.class.getName(), testContext.asyncAssertSuccess()))
         .test("testStart", testContext -> {
-          final Async async = testContext.async();
+          final var async = testContext.async();
           vertx
               .exceptionHandler(testContext.exceptionHandler())
               .createHttpClient()
-              .get(8080, "localhost", "/", response -> {
+              .get(RainConstants.SERVER_PORT, "localhost", "/", response -> {
                 testContext.assertEquals(200, response.statusCode());
                 response.bodyHandler(body -> {
-                  testContext.assertTrue(body.toString().contains("Reactive rain in Monte Carlo"));
+                  testContext.assertTrue(body.toString().contains("Reactive Rain in Monte Carlo"));
                   async.complete();
                 });
               })
